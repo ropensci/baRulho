fab20d73e7651fa3b26dcd4ca79f9774d6be9df3
Marcelo Araya-Salas <marcelo.araya@ucr.ac.cr>
2023-09-28 15:17:37 -0600

    increasing ocverage again

--- tests/testthat/test-master_sound_file.R
+++ tests/testthat/test-master_sound_file.R
@@ -9,5 +9,21 @@ test_that("save file", {
 
     expect_true(file.exists(file.path(tempdir(), "example_master.wav")))
       
+  unlink(file.path(tempdir(), "example_master.wav"))
+})
+
+
+test_that("no top.freq, bottom.freq, sound id columns ", {
+  
+  data("master_est")
+  
+  # create master sound file
+  master.sel.tab <- master_sound_file(
+    X = master_est[,1:4], file.name = "example_master",
+    dest.path = tempdir(), gap.duration = 0.3)
+  
+  expect_true(file.exists(file.path(tempdir(), "example_master.wav")))
+  
+  unlink(file.path(tempdir(), "example_master.wav"))
   
 })
--- tests/testthat/test-noise_profile.R
+++ tests/testthat/test-noise_profile.R
@@ -54,3 +54,26 @@ test_that("using data frame", {
   expect_equal(class(np)[1], "data.frame")
   
 })
+
+
+test_that("using entire files", {
+  data("test_sounds_est")
+  
+  # set temporary directory
+  td <- tempdir()  
+  
+  for (i in unique(test_sounds_est$sound.files)[2:3])
+    writeWave(object = attr(test_sounds_est, "wave.objects")[[i]], file.path(td, i))
+  
+  options(sound.files.path = td, pb = FALSE)
+  
+  np <-  
+    noise_profile(mar = 0.01, pb = FALSE)
+  
+  expect_equal(nrow(np), 60)
+  
+  expect_equal(ncol(np), 3)
+  
+  expect_equal(class(np)[1], "data.frame")
+  
+})

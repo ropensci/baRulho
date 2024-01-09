8b4ce4e733ad67aad8a83310547395bf4b931c0f
Marcelo Araya-Salas <marcelo.araya@ucr.ac.cr>
2024-01-08 18:22:41 -0600

    added all options wn argument

--- R/auto_realign.R
+++ R/auto_realign.R
@@ -1,38 +1,38 @@
-#' Fix misaligned start and end of test sounds
+#' Fix small misalignments in the time position test sounds
 #'
-#' \code{auto_realign} fixes misaligned start and end of test sounds in an extended selection table using spectrographic cross-correlation
+#' \code{auto_realign} fixes small misalignments in the time position of test sounds in an extended selection table using spectrographic cross-correlation
 #' @inheritParams template_params
 #' @param X object of class 'extended_selection_table' created by the function \code{\link[warbleR]{selection_table}} from the warbleR package. The object must include the following additional columns: 'sound.id', 'bottom.freq' and 'top.freq'.
+#' @param Y object of class 'extended_selection_table' (a class created by the function \code{\link[warbleR]{selection_table}} from the warbleR package) with the master sound file annotations. This should be the same data than that was used for finding the position of markers in \code{\link{find_markers}}. It should also contain a 'sound.id' column.
 #' @param ovlp Numeric vector of length 1 specifying the percentage of overlap between two
-#' consecutive windows, as in \code{\link[seewave]{spectro}}. Default is 90. High values of ovlp
-#' slow down the function but produce more accurate results.
-#' @param wn A character vector of length 1 specifying the window name as in \code{\link[seewave]{ftwindow}}.
+#' consecutive windows, as in \code{\link[seewave]{spectro}}. Default is 90. High values slow down the function but produce more accurate results.
 #' @return Object 'X' in which time parameters (columns 'start' and 'end') have been tailored to more closely match the start and end of the reference sound.
 #' @export
 #' @name auto_realign
-#' @details This function uses spectrographic cross-correlation to align the position in time of sounds with regard to a reference sound. The sound recorded at the closest distance to the source is used as reference. Precise alignment is crucial for downstream measures of sound degradation. The function calls warbleR's \code{\link[warbleR]{cross_correlation}} internally to align sounds using cross-correlation. The output extended selection table contains the new start and end values after alignment.
+#' @details Precise alignment is crucial for downstream measures of sound degradation. This function uses spectrographic cross-correlation to align the position in time of test sounds. The master sound file is used as reference. The function calls warbleR's \code{\link[warbleR]{cross_correlation}} internally to align sounds using cross-correlation. The output extended selection table contains the new start and end values after alignment. Note that this function only works to further improve alignments if the estimated position of the test sound is already close to the actual position. Note that both 'X' and 'Y' must be extended selection tables sensu \code{\link[warbleR]{selection_table}}.
+#' 
 #' @examples {
 #'   # load example data
 #'   data("test_sounds_est")
-#'
+#'   data("master_est")
+#'   
 #'   # create "unaligned_test_sounds_est" by
-#'   # adding noise to "test_sounds_est" start and end
+#'   # adding error to "test_sounds_est" start and end
 #'   unaligned_test_sounds_est <- test_sounds_est
 #'   set.seed(123)
-#'   noise_time <- sample(c(0.005, -0.005, 0.006, -0.006, 0, 0.002, -0.002),
-#'     nrow(unaligned_test_sounds_est),
-#'     replace = TRUE
-#'   )
+#'   noise_time <- sample(c(0.009, -0.01, 0.03, -0.03, 0, 0.07, -0.007),
+#'   sum(unaligned_test_sounds_est$distance != 0),
+#'   replace = TRUE)
+#'   
+#'   attr(unaligned_test_sounds_est, "check.res")$start <- 
+#'   unaligned_test_sounds_est$start <- 
+#'   unaligned_test_sounds_est$start + noise_time
+#'   attr(unaligned_test_sounds_est, "check.res")$end <- 
+#'   unaligned_test_sounds_est$end  <- 
+#'   unaligned_test_sounds_est$end + noise_time
 #'
-#'   attr(unaligned_test_sounds_est, "check.res")$start <-
-#'     unaligned_test_sounds_est$start <- unaligned_test_sounds_est$start + noise_time
-#'
-#'   attr(unaligned_test_sounds_est, "check.res")$end <- unaligned_test_sounds_est$end <-
-#'     unaligned_test_sounds_est$end + noise_time
-#'
-#'   # re align
-#'
-#'   realigned_est <- auto_realign(X = unaligned_test_sounds_est)
+#' # re align
+#' realigned_est <- auto_realign(X = unaligned_test_sounds_est, Y = master_est)
 #' }
 #'
 #' @author Marcelo Araya-Salas (\email{marcelo.araya@@ucr.ac.cr})
@@ -46,13 +46,17 @@
 
 auto_realign <-
   function(X,
-           parallel = NULL,
+           Y,
            cores = getOption("mc.cores", 1),
            pb = getOption("pb", TRUE),
            hop.size = getOption("hop.size", 11.6),
            wl = getOption("wl", NULL),
            ovlp = getOption("ovlp", 90),
-           wn = "hanning") {
+           wn = c("hanning", "hamming", "bartlett", "blackman", "flattop", "rectangle")) {
+    
+    # assign a value to wn
+    wn <- rlang::arg_match(wn)
+    
     # check arguments
     arguments <- as.list(base::match.call())
     
@@ -69,23 +73,27 @@ auto_realign <-
     .report_assertions(check_results)
     
     
+    # combine the two extended selection tables
+    common_cols <- intersect(names(X), names(Y))  
+    W <- rbind(X[, common_cols], Y[, common_cols])
+    
     # set number of processes for printing message
     if (pb) {
-      on.exit(options(warbleR.steps = 3), add = TRUE)
+      on.exit(options(warbleR.steps = 3))
       
       options(warbleR.steps = 3)
     }
     
     # adjust wl based on hop.size
-    wl <- .adjust_wl(wl, X, hop.size)
+    wl <- .adjust_wl(wl, W, hop.size)
 
     # remove ambient if any from sound types
-    sig.types <- setdiff(unique(X$sound.id), "ambient")
+    sig.types <- setdiff(unique(W$sound.id), c("ambient", "start_marker", "end_marker"))
     
     # create matrix containing pairwise comparisons of selections (2 columns)
     comp_mats <- lapply(sig.types, function(x) {
       # extract for single sound and order by distance
-      Y <- as.data.frame(X[X$sound.id == x,])
+      Y <- as.data.frame(W[W$sound.id == x,])
       
       # create selec ID column (unique ID for each selection (row))
       Y$sf.selec <- paste(Y$sound.files, Y$selec, sep = "-")
@@ -101,19 +109,19 @@ auto_realign <-
     # put together in a single
     comp_mat <- do.call(rbind, comp_mats)
     
-    # resave X
-    Y <- X
+    # resave W
+    Z <- W
     
     # get index of sounds that would be align
     indx.algn <-
-      which(paste(Y$sound.files, Y$selec, sep = "-") %in% comp_mat[, 1])
+      which(paste(Z$sound.files, Z$selec, sep = "-") %in% comp_mat[, 1])
     
     # fix end to include half the duration of the selection at both sides
-    attr(Y, "check.results")$end[indx.algn] <- Y$end[indx.algn] <-
+    attr(Z, "check.results")$end[indx.algn] <- Z$end[indx.algn] <-
       vapply(indx.algn, function(x) {
-        wv.info <- warbleR::read_wave(X, index = x, header = TRUE)
+        wv.info <- warbleR::read_wave(W, index = x, header = TRUE)
         mxdur <- wv.info$samples / wv.info$sample.rate
-        new.end <- X$end[x] + (X$end[x] - X$start[x]) * 0.7
+        new.end <- W$end[x] + (W$end[x] - W$start[x]) * 0.7
         
         if (mxdur < new.end) {
           return(mxdur)
@@ -123,13 +131,13 @@ auto_realign <-
       }, FUN.VALUE = numeric(1))
     
     # fix start in the same way
-    attr(Y, "check.results")$start[indx.algn] <-
-      Y$start[indx.algn] <-
-      X$start[indx.algn] - (X$end[indx.algn] - X$start[indx.algn]) * 0.7
+    attr(Z, "check.results")$start[indx.algn] <-
+      Z$start[indx.algn] <-
+      W$start[indx.algn] - (W$end[indx.algn] - W$start[indx.algn]) * 0.7
     
     # make 0 any negatives
-    attr(Y, "check.results")$start[Y$start < 0] <-
-      Y$start[Y$start < 0] <- 0
+    attr(Z, "check.results")$start[Z$start < 0] <-
+      Z$start[Z$start < 0] <- 0
     
     # save previous warbleR options
     prev_wl <- .Options$warbleR
@@ -141,7 +149,8 @@ auto_realign <-
         wn = prev_wl$wn,
         parallel = prev_wl$parallel,
         pb = prev_wl$pb
-      )
+      ),
+      add = TRUE
     )
     
     # steps for warbleR message
@@ -156,7 +165,7 @@ auto_realign <-
       parallel = cores,
       pb = pb,
       compare.matrix = comp_mat,
-      X = Y
+      X = Z
     )
     
     # run spcc
@@ -173,18 +182,21 @@ auto_realign <-
                   cores = cores,
                   max.peak = TRUE)
     
+    
+    # add column with original sound file selec labels
+    peaks$sound.files.selec <- comp_mat[, 1]
+    
     # fix start and end in original data set and its attributes
     # start
-    attr(X, "check.results")$start[paste(X$sound.files, X$selec, sep = "-") %in% comp_mat[, 1]] <-
-      X$start[paste(X$sound.files, X$selec, sep = "-") %in% comp_mat[, 1]] <-
-      peaks$start
+    for (i in peaks$sound.files.selec){
+      attr(X, "check.results")$start[paste(X$sound.files, X$selec, sep = "-") == i] <-
+      X$start[paste(X$sound.files, X$selec, sep = "-") == i] <- peaks$start[peaks$sound.files.selec == i]
     
-    # end
-    attr(X, "check.results")$end[paste(X$sound.files, X$selec, sep = "-") %in% comp_mat[, 1]] <-
-      X$end[paste(X$sound.files, X$selec, sep = "-") %in% comp_mat[, 1]] <-
-      X$start[paste(X$sound.files, X$selec, sep = "-") %in% comp_mat[, 1]] +
-      (peaks$end - peaks$start)
+      attr(X, "check.results")$end[paste(X$sound.files, X$selec, sep = "-") == i] <-
+        X$end[paste(X$sound.files, X$selec, sep = "-") == i] <- peaks$end[peaks$sound.files.selec == i]
+    }
     
+    # recalculate duration in attribute metadata
     attr(X, "check.results")$duration <-
       attr(X, "check.results")$end - attr(X, "check.results")$start
     
